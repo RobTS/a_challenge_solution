74835d5c9bee6e0b2ec76d95ee66ea677c47b664
RobTS <rob@rtsworks.com>
2020-08-18 14:18:57 +0200

    App: Reducer set up.

--- packages/app/src/state/reducers/profiles.ts
+++ packages/app/src/state/reducers/profiles.ts
@@ -1,12 +1,18 @@
 import { SafeActionType } from '../entities';
 import { ProfileReducerState } from '../reducerTypes';
+import { SAFE_CONSTANTS } from '../constants';
 
 export const profilesReducer = (
   state: ProfileReducerState,
   action: SafeActionType,
 ): ProfileReducerState => {
   switch (action.type) {
-    default:
-      return state;
+    case SAFE_CONSTANTS.FETCH_DATA_START:
+      return { type: 'loading' };
+    case SAFE_CONSTANTS.FETCH_DATA_SUCCESS:
+      return { type: 'success', data: action.payload };
+    case SAFE_CONSTANTS.FETCH_DATA_FAIL:
+      return { type: 'error', error: action.error };
   }
+  return state;
 };
